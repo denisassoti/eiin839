42cddc25ed76178c2fe446cabcdc0c6f70eda791
Lidaou Denis ASSOTI <58354924+denisassoti@users.noreply.github.com>
2022-02-16 17:48:03 +0100

    "2: Appel d'applications externes au serveur web" OK

--- TD2/BasicWebServer/MyMethods.cs
+++ TD2/BasicWebServer/MyMethods.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
 
 namespace BasicWebServer
 {
     internal class MyMethods
     {
-        public string Method1(string param1)
+        //question 1
+        public string Method1(string param1, string param2)
         {
             return "<html><body> Hello " + param1 +"</body></html>";
         }
-        public string Method2(string param1, string param2)
+
+        //question 2 
+        public string ExecTest(string param1, string param2)
         {
-            return "<html><body> Hello " + param1 + " et " + param2 + "</body></html>";
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = @"D:\COURS NICE\annee 4\S8\SOC\TD\eiin839\TD2\ExecTest\bin\Debug\ExecTest.exe";
+            start.Arguments = param1+" "+param2; 
+            
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            //
+            // Start the process.
+            //
+            using (Process process = Process.Start(start))
+            {
+                //
+                // Read in all the text from the process with the StreamReader.
+                //
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    return result;
+                    //Console.ReadLine();
+                }
+            }
         }
 
     }

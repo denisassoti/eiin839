f62707674603f2c5360eebe95337bde5997877aa
Lidaou Denis ASSOTI <58354924+denisassoti@users.noreply.github.com>
2022-02-25 21:48:20 +0100

    Session 1 : Your First REST Web Service Client OK

---
+++ TD3/TD3/Contract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD3
+{
+    public class Contract
+    {
+        public string name { get; set; }
+        public string commercial_name { get; set; }
+        public List<string> cities { get; set; }
+        public string country_code { get; set; }
+
+        public override string ToString()
+        {
+            return "name: " + name + " commercial_name: " + commercial_name + " cities: " + cities + " country_code: " + country_code;
+        }
+    }
+}

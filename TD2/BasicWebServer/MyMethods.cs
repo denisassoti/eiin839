11cb003cf89446a7222b1f0678b9e1f4cbafd345
Lidaou Denis ASSOTI <58354924+denisassoti@users.noreply.github.com>
2022-02-26 23:48:53 +0100

    bonus : execution d'un script python comme programme externe

--- TD2/BasicWebServer/MyMethods.cs
+++ TD2/BasicWebServer/MyMethods.cs
@@ -14,7 +14,7 @@ namespace BasicWebServer
         //question 1
         public string Method1(string param1, string param2)
         {
-            return "<html><body> Hello " + param1 +"</body></html>";
+            return "<html><body> Hello " + param1+ " "+ param2 + "</body></html>";
         }
 
         //question 2 
@@ -39,6 +39,29 @@ namespace BasicWebServer
                     string result = reader.ReadToEnd();
                     return result;
                     //Console.ReadLine();
+                }
+            }
+        }
+
+        //question 2 Bonus python script call
+        public string ExecPython(string param1, string param2)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            // here we execute the python interpreter exe instead of the exe generate by a c# program 
+            start.FileName = @"C:\Users\denis\AppData\Local\Programs\Python\Python310\python.exe";
+
+            // here we specify the python script to be executed and the parameters
+            var script = @"D:\COURS NICE\annee 4\S8\SOC\TD\eiin839\TD2\BasicWebServer\script.py";
+            start.Arguments = $"\"{script}\" \"{param1}\" \"{param2}\"";
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    return result;
                 }
             }
         }
